b686dd4d990b4b205286c9fe918911c287aae498
Patrick Mhando <patrckmhando@gmail.com>
2020-01-18 00:42:59 +0300

    none responsive site

---
+++ script.js
@@ -0,0 +1,74 @@
+// const quiz=[
+//     ['What is your name?','Patrick Mhando'],
+//     ['How older are you?','29'],
+//     ['Are you married?','No']
+// ];
+// let score=0;
+// for(const[question,answer] of quiz){
+//     const response=prompt(question);
+//     if(response === answer){
+//         alert('Correct');
+//         score++;
+//     }else{
+//         alert(`Wrong!!! The answer was ${answer}`);
+//     }
+// }
+// alert(`game over your score is ${score} point${score !==1? 's' :' '}`);
+
+
+// function mean(... values){
+//     let total = 0;
+//     for(const value of values){
+//         total+=value;
+//     }
+//     return total/values.length;
+// }
+// function hey(name='patrick'){
+//     console.log(`hello ${name}`);
+// }
+
+// const myArrow=(x,y) =>{
+//     return x*y;
+// }
+// function mine(name,wife){
+//     let hers="Glady";
+//     console.log(`${name}`);
+//     wife(hers);
+// }
+// function wife(her){
+//     console.log(`love you ${her}`);
+// }
+
+// const myArray=[3,5,8,1,4,2,9,7,8,11,16,20];
+
+// function numerically(a,b){
+//     if(a<b){
+//         return -1;
+//     }else if(a>b){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// }
+
+const myColors=['Red','White','Blue','Orange'];
+// for(let i=0; i<myColors.length; i++){
+//     console.log(`color at ${i} is ${myColors[i]}`);
+// }
+
+// myColors.forEach((color,index)=>
+//     console.log(`color at ${index} is ${color} `));
+
+// const myNumbers=[1,2,3,4,5,6,7,8,9, ''];
+
+
+// function square(x){
+//     return x * x;
+// }
+// const mySecond='My name is Patrick Mhando from kwamwenda';
+
+const today= new Date();
+
+
+
+
